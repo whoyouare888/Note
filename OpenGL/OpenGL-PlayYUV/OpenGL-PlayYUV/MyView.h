abf76db4bf1e98b7ed8ee267e4f61138c1d7747b
whoyouare888 <17710050160@163.com>
2019-07-22 11:20:54 +0800

    f

---
+++ OpenGL/OpenGL-PlayYUV/OpenGL-PlayYUV/MyView.h
@@ -0,0 +1,17 @@
+//
+//  MyView.h
+//  OpenGL-PlayYUV
+//
+//  Created by zhw on 2019/7/21.
+//  Copyright © 2019 zhw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OpenGL/OpenGL-PlayYUV/OpenGL-PlayYUV/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  OpenGL-PlayYUV
+//
+//  Created by zhw on 2019/7/21.
+//  Copyright © 2019 zhw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
