68f7a07dbe67eb49c1ca534e5bd41f58de1c7fc8
zhw <17710050160@163.com>
2019-08-22 14:18:38 +0800

    vt

---
+++ VideoToolbox/VTEncode/VTEncode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VTEncode
+//
+//  Created by zhw on 2019/8/12.
+//  Copyright © 2019 zhw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoToolbox/VTEncode/VTEncode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VTEncode
+//
+//  Created by zhw on 2019/8/12.
+//  Copyright © 2019 zhw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
